9008f690ce6510caa1f3be2dc40fd7efb7334666
pacofer71 <pacofer71@gmail.com>
2021-11-02 10:17:13 +0100

    Libros 02/11/2021

--- src/Autores.php
+++ src/Autores.php
@@ -113,14 +113,61 @@ class Autores extends Conexion{
         $q="select * from autores";
         $stmt=parent::$conexion->prepare($q);
         try{
-            $stmt->execute();
-        }catch(PDOException $ex){
-            die("Error en hay autores: ".$ex->getMessage()); 
-        }
+            $stmt->execute();<!DOCTYPE html>
+            <html lang="es">
+            
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <!-- BootStrap -->
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                <!-- FONTAWESOME -->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            
+                <title></title>
+            
+            
+            
+            
+            
+            </head>
+            
+            <body style="background-color:silver">
+                <h3 class="text-center"></h3>
+                <div class="container mt-2">
+            
+                </div>
+            </body>
+            
+            </html>
         parent::$conexion=null;
         return $stmt->rowCount(); //devuelve el numero de filas 
 
     }
+    public function devolverId(){
+        $q="select id from autores order by id";
+        $stmt=parent::$conexion->prepare($q);
+        try{
+            $stmt->execute();
+        }catch(PDOException $ex){
+            die("Error en el metodo devolver id: ".$ex->getMessage());
+        }
+        $id=[];
+        while($fila=$stmt->fetch(PDO::FETCH_OBJ)){
+            $id[]=$fila->id;
+        }
+        parent::$conexion=null;
+        return $id;
+
+    }
+
+
+
+
+
+
+
 
 
     /**
---
+++ src/Libros.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Libreria;
+
+use PDOException;
+use Faker;
+
+class Libros extends Conexion
+{
+    private $id;
+    private $titulo;
+    private $sinopsis;
+    private $isbn;
+    private $autor_id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    //-------------------- CRUD ---------------------
+    public function create()
+    {
+        $q = "insert into libros(titulo, sinopsis, isbn, autor_id) values(:t, :s, :isbn, :ai)";
+        $stmt = parent::$conexion->prepare($q);
+        try {
+            $stmt->execute([
+                ':t' => $this->titulo,
+                ':s' => $this->sinopsis,
+                ':isbn' => $this->isbn,
+                ':ai' => $this->autor_id
+            ]);
+        } catch (PDOException $ex) {
+            die("Error al insertar libro: " . $ex->getMessage());
+        };
+        parent::$conexion = null;
+    }
+    public function read()
+    {
+    }
+    public function update()
+    {
+    }
+    public function delete()
+    {
+    }
+    public function readAll(){
+        $q="select * from libros order by titulo, autor_id";
+        $stmt=parent::$conexion->prepare($q);
+        try{
+            $stmt->execute();
+        }catch(PDOException $ex){
+            die("Error al devolver todos los libros: ".$ex->getMessage());
+        }
+        parent::$conexion=null;
+        return $stmt;
+    }
+
+    //---------------------Otros Métodos------------
+    public function generarLibros($cant)
+    {
+        if (!$this->hayLibros()) {
+            $faker = Faker\Factory::create('es_ES');
+            $autores = (new Autores)->devolverId(); //[2, 3, 4, 8, 12 ...]
+
+            for ($i = 0; $i < $cant; $i++) {
+                $titulo = $faker->sentence(4);
+                $sinopsis = $faker->text(200);
+                $isbn = $faker->unique()->isbn13();
+                $autor_id = $autores[array_rand($autores, 1)];
+
+                (new Libros)->setTitulo($titulo)
+                    ->setSinopsis($sinopsis)
+                    ->setIsbn($isbn)
+                    ->setAutor_id($autor_id)
+                    ->create();
+            }
+        }
+    }
+    public function hayLibros()
+    {
+        $q = "select * from libros";
+        $stmt = parent::$conexion->prepare($q);
+        try {
+            $stmt->execute();
+        } catch (PDOException $ex) {
+            die("Error al comprobar si hay libros: " . $ex->getMessage());
+        }
+        $totalLibros = $stmt->rowCount();
+        parent::$conexion = null;
+        return ($totalLibros > 0);
+    }
+
+    //----------------------------------------------------
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of titulo
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @return  self
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sinopsis
+     */
+    public function getSinopsis()
+    {
+        return $this->sinopsis;
+    }
+
+    /**
+     * Set the value of sinopsis
+     *
+     * @return  self
+     */
+    public function setSinopsis($sinopsis)
+    {
+        $this->sinopsis = $sinopsis;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of isbn
+     */
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * Set the value of isbn
+     *
+     * @return  self
+     */
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of autor_id
+     */
+    public function getAutor_id()
+    {
+        return $this->autor_id;
+    }
+
+    /**
+     * Set the value of autor_id
+     *
+     * @return  self
+     */
+    public function setAutor_id($autor_id)
+    {
+        $this->autor_id = $autor_id;
+
+        return $this;
+    }
+}
