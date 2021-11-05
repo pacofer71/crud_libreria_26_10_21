74bdc46fed6908b7ae5ec046b0faaffca5c7c256
pacofer71 <pacofer71@gmail.com>
2021-11-05 09:46:41 +0100

    05/11/2021 Datatable index Libros

--- public/libros/index.php
+++ public/libros/index.php
@@ -23,6 +23,7 @@ $stmt = (new Libros)->readAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- BootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <!-- FONTAWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -43,7 +44,7 @@ $stmt = (new Libros)->readAll();
             }
         ?>
         <a href="clibro.php" class="btn btn-primary mb-2"><i class="fas fa-book-medical"></i> Nuevo Libro</a>
-        <table class="table table-info table-striped">
+        <table class="table table-info table-striped" id="tablalibros">
             <thead>
                 <tr>
                     <th scope="col">Detalle</th>
@@ -78,6 +79,17 @@ $stmt = (new Libros)->readAll();
         </table>
 
     </div>
+    <script
+			  src="https://code.jquery.com/jquery-3.6.0.min.js"
+			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+			  crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+        $('#tablalibros').DataTable();
+} );
+    </script>
 </body>
 
 </html>
